10967d97643c0755121aa53733b2e23e929d3e8d
Alex C <dreaming.about.electric.sheep@gmail.com>
2024-02-01 02:14:25 +0000

    Fix bug with polybar launch

--- i3/.config/polybar/launch.sh
+++ i3/.config/polybar/launch.sh
@@ -10,7 +10,7 @@ polybar-msg cmd quit
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 #polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 #polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
-polybar bartop 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar barmajor 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
 
