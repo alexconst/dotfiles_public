f5e07afad51204398cda6241b1e3f97bc597c555
Alex C <dreaming.about.electric.sheep@gmail.com>
2024-02-06 12:00:53 +0000

    Refactor screen-lock.sh

--- i3/.config/i3/scripts/screen-lock.sh
+++ i3/.config/i3/scripts/screen-lock.sh
@@ -2,68 +2,102 @@
 
 # ISSUES:
 # - the only way to stop the lock process is by pressing the Esc key. No other key nor mouse move or button will work. That is a feh limitation.
-# - there is a race condition between notify-send and feh. feh is too slow
-# - the notify-send popup will linger until the timeout unless you click on it. ie, pressing Esc won't remove it
+# - there is a race condition between notify-send and feh. feh is too slow, so the popup may not be visible on some random occasion
 
 
-timeout="$1"
-locker="${@:2} -n"              # make i3lock run without forking
-image="${@: -1}"
-
+TIMEOUT_SECS="$1"
+LOCKER="${@:2} --nofork"
+IMAGE="${@: -1}"
+logfile="/tmp/screen-lock.log"
 
 function error_msg() {
-    message="$@"
-    xmessage -geometry 200x200 -center -buttons EXIT:1  "$message"
+    local msg="$@"
+    xmessage -geometry 200x200 -center -buttons EXIT:1  "$msg"
+}
+
+function logger_debug() {
+    return # NOTE: disabled
+
+    local msg="$@"
+    echo "$(date) DEBUG: $msg" >> $logfile
+}
+
+function display_image_fullscreen() {
+    local image="$1"
+    # launch feh process in the background, to display image in fullscreen mode as a preview of the screenlock itself
+    feh --fullscreen --zoom 100 "${image}" &
+    # Get the process ID of feh
+    FUN_RET=$!
+    logger_debug "launched feh"
+}
+
+function notify() {
+    # because notify-send is short lived, ie the process finishes while the popup itself persists, we can't use the pid, but we can workaround it using its notification id
+    local timeout_secs="$1"
+    local action="$2"   # can be: print, replace
+    local handle="$3"
+    if [[ "$action" == "print" ]]; then
+        timeout_ms="${timeout_secs}000"
+        action="-p"
+    elif [[ "$action" == "replace" ]]; then
+        timeout_ms=1
+        action="-r $handle"
+    else
+        msg="invalid operation: $action"
+        logger_debug "$msg"
+        error_msg "$msg"
+    fi
+    logger_debug "notify action: $action"
+    FUN_RET=$(notify-send --transient --expire-time ${timeout_ms} "PSA" "Will lock screen in $timeout_secs seconds... Press Esc to abort" $action)
+    logger_debug "notify output: $FUN_RET"
 }
 
 unset msg
-if [[ -z "$timeout" ]]; then
+if [[ -z "$TIMEOUT_SECS" ]]; then
     msg="no timeout provided"
-elif [[ ! -f "$image" ]]; then
-    msg="no such image: $image"
-elif [[ ! $locker =~ i3lock ]]; then
+elif [[ ! -f "$IMAGE" ]]; then
+    msg="no such image: $IMAGE"
+elif [[ ! $LOCKER =~ i3lock ]]; then
     msg="invalid i3locker command"
 fi
 if [[ -n "$msg" ]]; then
-    echo "$(date) DEBUG: $msg" >> /tmp/tmp.txt
+    logger_debug "$msg"
     error_msg "$msg"
     exit 1
 fi
 
 
-echo "$(date) DEBUG: timeout = $timeout" >> /tmp/tmp.txt
-echo "$(date) DEBUG: image = $image" >> /tmp/tmp.txt
-echo "$(date) DEBUG: locker = $locker" >> /tmp/tmp.txt
+logger_debug "timeout = $TIMEOUT_SECS"
+logger_debug "image = $IMAGE"
+logger_debug "locker = $LOCKER"
 
-# Run feh in fullscreen mode in the background
-feh --fullscreen --zoom 100 "${image}" &
-echo "$(date) DEBUG: launched feh" >> /tmp/tmp.txt
-# Get the process ID of feh
-pid=$!
-# NOTE: race condition... when avoiding feh to obscure notify-send
+
+# display full screen image
+display_image_fullscreen "$IMAGE"
+pid_feh=$FUN_RET
+
+# because notify-send is faster than feh it would get occluded by it, one fix is to just wait, but notice this makes for a race condition
 sleep 1s
+# show popup saying it will lock the screen
+notify "$TIMEOUT_SECS" "print"
+nid_notify=$FUN_RET
 
-# NOTE: this logic actually doesn't work because notify-send is short lived, ie it's process finishes even though the pop up persists
-# Leaving this logic here with the hope it may be fixed with future notify-send versions
-notid=$(notify-send --transient --expire-time ${timeout}000 "PSA" "Will lock screen in $timeout seconds... Press Esc to abort" -p)
-#pidnot=$!
 # wait for $timeout seconds before locking screen and if the user acts then kill the message popup
 elapsed_time=0
-while [[ $elapsed_time -lt $timeout ]]; do
+while [[ $elapsed_time -lt $TIMEOUT_SECS ]]; do
     sleep 1
     let elapsed_time+=1
-    if [[ ! -d /proc/$pid ]]; then
-        # if feh was closed then:
-        #kill $pidnot            # close notify-send popup
-        notify-send --transient --expire-time 1 "PSA" "Will lock screen in $timeout seconds... Press Esc to abort" -r $notid
-        break                   # break loop
+    if [[ ! -d /proc/$pid_feh ]]; then
+        # if feh was closed then we can also terminate the popup notification
+        notify "$TIMEOUT_SECS" "replace" "$nid_notify"
+        break
     fi
 done
 
 # if feh is still running after the timeout then we screen lock
-if [[ -d /proc/$pid ]]; then
-    eval $locker                # run i3lock
-    kill $pid                   # kill feh
+if [[ -d /proc/$pid_feh ]]; then
+    eval $LOCKER                # run i3lock
+    kill $pid_feh               # kill feh
 fi
 
 
